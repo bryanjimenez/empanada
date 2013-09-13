143061bfd25b11c78fc1ebb301009828b67269dd
Bryan Jimenez <bjime008@fiu.edu>
2013-09-13 16:58:41 -0400

    harcoded tweets for test

--- refresh.php
+++ refresh.php
@@ -1,6 +1,11 @@
 <?php
 
-echo '{"markers": [{"geo": "25.75906,-80.37388","text": "From the server with love!"},{"geo": "25.75977,-80.37157","text": "Dunkin!"},{"geo": "25.75873,-80.37246","text": "Chem/Phys!"},{"geo": "25.75838,-80.37365","text": "Wet!"},{"geo": "25.75964,-80.37553","text": "Wet!"}]}';
+echo '{"markers": [
+{"user": "one","img": "https://1.gravatar.com/avatar/1ee26eb5d1e53a3b9c5e45693c68ae1f?d=https%3A%2F%2Fidenticons.github.com%2Fe1895e3d1b333d255d3c801a228f8165.png&s=105","geo": "25.75906,-80.37388","text": "From the server with love!"},
+{"user": "one","img": "https://1.gravatar.com/avatar/1ee26eb5d1e53a3b9c5e45693c68ae1f?d=https%3A%2F%2Fidenticons.github.com%2Fe1895e3d1b333d255d3c801a228f8165.png&s=105","geo": "25.75977,-80.37157","text": "Dunkin!"},
+{"user": "one","img": "https://1.gravatar.com/avatar/1ee26eb5d1e53a3b9c5e45693c68ae1f?d=https%3A%2F%2Fidenticons.github.com%2Fe1895e3d1b333d255d3c801a228f8165.png&s=105","geo": "25.75873,-80.37246","text": "Chem/Phys!"},
+{"user": "one","img": "https://1.gravatar.com/avatar/1ee26eb5d1e53a3b9c5e45693c68ae1f?d=https%3A%2F%2Fidenticons.github.com%2Fe1895e3d1b333d255d3c801a228f8165.png&s=105","geo": "25.75838,-80.37365","text": "Wet!"},
+{"user": "one","img": "https://1.gravatar.com/avatar/1ee26eb5d1e53a3b9c5e45693c68ae1f?d=https%3A%2F%2Fidenticons.github.com%2Fe1895e3d1b333d255d3c801a228f8165.png&s=105","geo": "25.75964,-80.37553","text": "Wet!"}]}';
 
 /*
 echo '{'geo':'25.75906,-80.37388','text':'From the server with love!'}';
