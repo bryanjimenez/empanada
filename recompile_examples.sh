4f28cf9961538bd6cdc096b9bee1bd3311d6d836
Bryan Jimenez <bjime008@fiu.edu>
2013-09-24 09:10:39 -0400

    fixed JAVA_HOME

--- recompile_examples.sh
+++ recompile_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
 
 javac -classpath hadoop-1.2.1/hadoop-core-1.2.1.jar:hadoop-1.2.1/lib/commons-cli-1.2.jar -d playground/classes/ playground/src/WordCount.java
 
