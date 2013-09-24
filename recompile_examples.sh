088e1de302aaf449acbe8caf3c87cb532926cf16
Bryan Jimenez <bjime008@fiu.edu>
2013-09-24 00:17:22 -0400

    +notes

--- recompile_examples.sh
+++ recompile_examples.sh
@@ -5,3 +5,6 @@ export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
 javac -classpath hadoop-1.2.1/hadoop-core-1.2.1.jar:hadoop-1.2.1/lib/commons-cli-1.2.jar -d playground/classes/ playground/src/WordCount.java
 
 jar -cvf playground/wordcount.jar -C playground/classes/ .
+
+# To run the newly compiled map reduce run
+# hadoop-1.2.1/bin/hadoop jar playground/wordcount.jar org.apache.hadoop.examples.WordCount <input> <output>
