beb11669c09991dbabd6c2b95b46f34aca015a0b
Santiago Fuertes <jonesxp@aim.com>
2013-09-27 12:58:39 -0400

    Some changes. Trying to post. Not successful. Getting 401 ERROR

--- chatbot/chatbox.py
+++ chatbot/chatbox.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from twython import Twython
+from twython import Twython, TwythonError
 import time
 
 def EMPa():
@@ -8,16 +8,23 @@ def EMPa():
 	ACCESS_TOKEN = "392397306-fgFNbrKfccVImUMLe64d6cgdy4gxffR8Tqc1pTCT"
 	ACCESS_TOKEN_SECRET = "D2xjntYtbhL3jlPJwrKOhaYkxI99r5B1XWzNFMFc"
 	
-	
-	twitter = Twython(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)		# creates an instance of twython that allows access to
-	
+	twitter = Twython(APP_KEY, APP_SECRET, oauth_version=1)
+	oauth = twitter.get_authentication_tokens()
+	twitter = Twython(APP_KEY, APP_SECRET, oauth['oauth_token'], oauth['oauth_token_secret'],
+					ACCESS_TOKEN)
+			
+	try:
+		twitter.update_status(status='See how easy this was?')
+	except TwythonError as e:
+		print e
+		
 	"""
 	twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 	ACCESS_TOKEN = twitter.obtain_access_token()
 	twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 	"""
 	
-	"""search = twitter.search_gen("python")"""
+	"""search = twitter.search_gen("python")
 
 	search = twitter.get_home_timeline()
 			
@@ -25,12 +32,11 @@ def EMPa():
 		print result['text']
 			
 	time.sleep(4)
-
+	"""
 
 def test():
-	print "1"
 	time.sleep(10)
-	print "2"
+
 		
 
 if __name__ == "__main__":
