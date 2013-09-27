0caaf610808cc0979f6ff81eea2917b070fcc33f
Santiago Fuertes <jonesxp@aim.com>
2013-09-27 16:08:00 -0400

    Got posting working for my account

--- chatbot/chatbox.py
+++ chatbot/chatbox.py
@@ -3,36 +3,34 @@ from twython import Twython, TwythonError
 import time
 
 def EMPa():
-	APP_KEY = 'i8yi6oyp4UtwoSGVBrqiVQ'
-	APP_SECRET = 'SXGkWmdvjRYvWRCHUDhJSTkkI5uVhXkm8xHjgCdSw'
-	ACCESS_TOKEN = "392397306-fgFNbrKfccVImUMLe64d6cgdy4gxffR8Tqc1pTCT"
-	ACCESS_TOKEN_SECRET = "D2xjntYtbhL3jlPJwrKOhaYkxI99r5B1XWzNFMFc"
+	import json
 	
-	twitter = Twython(APP_KEY, APP_SECRET, oauth_version=1)
-	oauth = twitter.get_authentication_tokens()
-	twitter = Twython(APP_KEY, APP_SECRET, oauth['oauth_token'], oauth['oauth_token_secret'],
-					ACCESS_TOKEN)
+	twitter_data = open('oauth.json')
+	oauth = json.load(twitter_data)
+	twitter_data.close()
+	
+	twitter = Twython(oauth['APP_KEY'], oauth['APP_SECRET'], oauth['ACCESS_TOKEN'], oauth['ACCESS_TOKEN_SECRET'])
+	twitter.verify_credentials()
 			
 	try:
-		twitter.update_status(status='See how easy this was?')
+		pass # twitter.update_status(status='See how easy this was?')
 	except TwythonError as e:
 		print e
 		
-	"""
+	""" # authentication 2 method
 	twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 	ACCESS_TOKEN = twitter.obtain_access_token()
 	twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 	"""
 	
-	"""search = twitter.search_gen("python")
-
-	search = twitter.get_home_timeline()
-			
+	# search = twitter.search_gen("python")
+	# search = twitter.get_home_timeline()
+	"""		
 	for result in search:
 		print result['text']
-			
-	time.sleep(4)
-	"""
+	"""		
+	
+	
 
 def test():
 	time.sleep(10)
