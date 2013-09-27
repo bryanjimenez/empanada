59f3fc93ead68e010e1dc566a5fb95d235473ac3
Santiago Fuertes <jonesxp@aim.com>
2013-09-27 16:37:57 -0400

    Added a streamer, posting and search_for method to the code

--- chatbot/chatbox.py
+++ chatbot/chatbox.py
@@ -1,41 +1,65 @@
 #!/usr/bin/python
-from twython import Twython, TwythonError
+from twython import Twython, TwythonError, TwythonStreamer
 import time
 
-def EMPa():
+class MyStreamer(TwythonStreamer):
+	
+	def on_success(self, data):
+    		if 'text' in data:
+        		print data['text'].encode('utf-8')
+
+	def on_error(self, status_code, data):
+		print status_code
+
+		# Want to stop trying to get data because of the error?
+		# Uncomment the next line!
+		# self.disconnect()
+
+def get_oauthentication():
 	import json
 	
 	twitter_data = open('oauth.json')
 	oauth = json.load(twitter_data)
 	twitter_data.close()
 	
+	return oauth
+
+
+def Update_status_with(oauth, message):
+		
 	twitter = Twython(oauth['APP_KEY'], oauth['APP_SECRET'], oauth['ACCESS_TOKEN'], oauth['ACCESS_TOKEN_SECRET'])
 	twitter.verify_credentials()
 			
 	try:
-		pass # twitter.update_status(status='See how easy this was?')
+		pass # twitter.update_status(status=message)
 	except TwythonError as e:
 		print e
 		
-	""" # authentication 2 method
-	twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-	ACCESS_TOKEN = twitter.obtain_access_token()
-	twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-	"""
 	
-	# search = twitter.search_gen("python")
-	# search = twitter.get_home_timeline()
-	"""		
-	for result in search:
-		print result['text']
-	"""		
-	
-	
+def Streamer():
+	oauth = get_oauthentication()
+	stream = MyStreamer(oauth['APP_KEY'], oauth['APP_SECRET'], oauth['ACCESS_TOKEN'], oauth['ACCESS_TOKEN_SECRET'])
+	stream.statuses.filter(track='gas')
+
 
 def test():
 	time.sleep(10)
 
-		
+
+def Search_for(str):
+	oauth = get_oauthentication()
+	twitter = Twython(oauth['APP_KEY'], oauth['APP_SECRET'], oauth_version=2)
+	ACCESS_TOKEN = twitter.obtain_access_token()
+	twitter = Twython(oauth['APP_KEY'], access_token=ACCESS_TOKEN)
+	
+	search = twitter.search_gen(str)
+	# search = twitter.get_home_timeline()
+			
+	for result in search:
+		print result['text']
+	
 
 if __name__ == "__main__":
-    EMPa()
+    Streamer()
+    # Search_for("gas")
+    # Update_status_with(get_oauthentication(), "This message")
