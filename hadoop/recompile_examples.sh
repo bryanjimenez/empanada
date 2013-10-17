22c3190d51d61e98ebaec397edecdc39edcbac6d
Bryan Jimenez <bjime008@fiu.edu>
2013-10-17 16:02:56 -0400

    more cleanup

--- hadoop/recompile_examples.sh
+++ hadoop/recompile_examples.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source env_vars
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
+export HADOOP_CLASSPATH=json-simple-1.1.1.jar
 
 javac -classpath hadoop-1.2.1/hadoop-core-1.2.1.jar:hadoop-1.2.1/lib/commons-cli-1.2.jar:./json-simple-1.1.1.jar -d playground/classes/ playground/src/WordCount.java
 
