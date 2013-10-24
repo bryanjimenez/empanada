a616acc4cc200e923ab0e939caa7b9b0e479a648
Santiago Fuertes <jonesxp@aim.com>
2013-10-24 15:10:42 -0400

    Fixed add_mention function to add only tweets that mention empanada305 and they containt one of the keywords form filters.json

--- chatbot/ChatBotClass.py
+++ chatbot/ChatBotClass.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 class ChatBot():
     def __init__(self):
+        self.debug = True
+        
+        self.website_url = "empanada.cs.fiu.edu"
         self.mentions = []
+        self.keys = []
+        self.radius = "4"
+        self.current_filter = "" # current filter
+        self.current_mention = {} # current mention
         self.question = False
         self.filter = self.get_filters()
         self.keyword = '@empanada305'
@@ -12,6 +19,7 @@ class ChatBot():
         self.message  = "Please refer to " + self.website_address + " for more information."
         self.general_message_no_location = "Your location is not available. Please refer to " + self.website_address + " for more information."
         self.you_mentioned_filter = "You mentioned something about "
+
         
 
     # read filters from json in http://empanada.cs.fiu.edu/json/filters.json
@@ -21,10 +29,10 @@ class ChatBot():
         import httplib
         
         json_filters = {}        
-        conn = httplib.HTTPConnection("empanada.cs.fiu.edu")
+        conn = httplib.HTTPConnection(self.website_url)
         conn.request("GET", "/json/filters.json")
         response = conn.getresponse()
-        if (response == "OK"):
+        if (response.reason == "OK"):
             data = response.read()
             json_filters = json.loads(data)
             
@@ -32,34 +40,91 @@ class ChatBot():
         return json_filters
     
 
+
     # adds incoming tweets to metions if the contain the self.keyword phrase
     # it won't get added if the tweet came from the same username
+    # right now i'm just checking that the tweet mentions @empanada305
+    # we can remove this later to analyze all incoming tweets
     def add_to_mention(self, tweet):
-        if ( tweet['user']['screen_name'] == self.twitter_username ):
-            print "DEBUG - " + tweet['user']['screen_name'] + " came from same user"
+        if ( tweet['user']['screen_name'] == self.twitter_username ): # if tweet is from the same username then we ignore the tweet
+            if self.debug: print "DEBUG - " + tweet['user']['screen_name'] + " came from same user"
             return -1 # tweet won't get a reply
-        for key in self.keyword:
-            if ( key.upper() in tweet['text'].upper() ):
-                print "DEBUG - added the tweet to the queue"
-                self.mentions.append(tweet)
-                return 0; # tweet had the self.keyword in it
+        # for key in self.keyword: # this could be remove or we could add more keywords
+        if ( str.upper(self.keyword) in tweet['text'].upper() ): # if the tweet mentions @empanada305 then we move on to find the filter in the text
+            for filter_key in self.filter:
+                if ( filter_key.upper() in tweet['text'].upper() ):
+                    if self.debug: print "DEBUG - added the tweet to the queue"
+                    self.mentions.append(tweet)
+                    self.keys.append(filter_key)
+                    return 0; # tweet had the self.keyword in it
 
         return -1 # tweet won't get a reply   
+
 
 
     # respond to mention based on a few details
     def respond_to_mention(self):
         if self.mentions:
-            mention = self.mentions.pop(0) # single mention variable
-            print "DEBUG - Geolocation is " + ("" if mention['user']['geo_enabled'] else "not ") + "enabled" # Debbugin
+            self.current_mention = self.mentions.pop(0) # single mention variable
+            self.current_filter = self.keys.pop(0) # single key variable corresponding to mention that was just poped
+            if self.debug: print "DEBUG - Geolocation is " + ("" if self.current_mention['user']['geo_enabled'] else "not ") + "enabled" # Debbugin
             # print type(mention['user']['geo_enabled'])
-            if ( mention['user']['geo_enabled'] == False ):
-                self.message = "@" + mention['user']['screen_name'] + " " + self.general_message_no_location
+            if ( self.current_mention['user']['geo_enabled'] == False ):
+                self.message = "@" + self.current_mention['user']['screen_name'] + " " + self.general_message_no_location
             else:
-                for item in self.filter:
-                    if ( item in mention['text'] ):
-                        # Make a querry to get incidents around the location?????
-                        self.message = "@" + mention['user']['screen_name'] + " " + self.you_mentioned_filter + item
+                # make a querry to get incidents around the location?????                    
+                # call to generate result - this makes a call to refresh wish returns issues around a longitude and latitude 
+                result_ok = self.generate_result()
+                # create message based on generate_result()
+                if (result_ok): self.message = generate_message(item) # "@" + self.current_mention['user']['screen_name'] + " " + self.you_mentioned_filter + item
+
+
+    # get_latitude form the current mention
+    def get_latitude(self):
+        try:
+            return str(self.current_mention['geo']['coordinates'][0])
+        except TypeError:
+            return -1
+    
+    
+    # get_longitude from the current tweet
+    def get_longitude(self):
+        try:
+            return str(self.current_mention['geo']['coordinates'][1])
+        except TypeError:
+            return -1
+        
+
+    #
+    def generate_message(self, item):
+        return "@" + self.current_mention['user']['screen_name'] + " " + self.you_mentioned_filter + item
+
+
+    # get results from backend
+    # right now i'll be getting results from the same place bryan is getting his results for the UI
+    def generate_result(self):
+        import json
+        import httplib
+        
+        latitude = self.get_latitude()
+        longitude = self.get_longitude()
+        filter = self.current_filter
+        
+        if (latitude == -1 and longitude == -1):
+            return False
+            
+        conn = httplib.HTTPConnection(self.website_url)
+        conn.request("GET", "/refresh.php?lat="+latitude+"&lng="+longitude+"&rad="+radius+"&olat=0&olng=0&orad=0&filter=fuel,"+filter)
+        result = conn.getresponse()
+        if self.debug: print "DEBUG - RESPONSE FROM getresponse() - " + result.reason
+        if (result.reason == "OK"):
+            data = result.read()
+            if self.debug: print "DEBUG - " + data
+            json_data = json.loads(data)
+            if self.debug: print "DEBUG - " + json_data['t'][0]['text']
+            
+        conn.close()
+        return True
 
 
 
--- chatbot/test.py
+++ chatbot/test.py
@@ -1,8 +1,9 @@
-def main():
+def json_read():
     import httplib
     import json
        
 
+    json_filter = {}
     h1 = httplib.HTTPConnection("empanada.cs.fiu.edu")
     h1.request("GET", "/json/filters.json")
     result = h1.getresponse()
@@ -14,9 +15,60 @@ def main():
     h1.close()
     
     for key in json_filter:
-        print key.upper()
-    
+        print key
 
+
+def results_read():
+    import json
+    import httplib
+    
+    conn = httplib.HTTPConnection("empanada.cs.fiu.edu")
+    conn.request("GET", "/refresh.php?lat=25.764084501106787&lng=-80.37422332275389&rad=4&olat=0&olng=0&orad=0&filter=fuel,zombies")
+    result = conn.getresponse()
+    print result.reason
+    if (result.reason == "OK"):
+        data = result.read()
+        # print data
+        json_data = json.loads(data)
+        print json_data['t'][0]['text']
+        print json_data['t'][0]['geo']['coordinates'][0]
+        print json_data['t'][0]['geo']['coordinates'][1]
+        
+    conn.close()
+
+
+def generate_result():
+    import json
+    import httplib
+    
+    radius = 4
+    latitude = 25.764084501106787
+    longitude = -80.37422332275389
+    filter = current_filter = "zombies"
+    
+    conn = httplib.HTTPConnection("empanada.cs.fiu.edu")
+    conn.request("GET", "/refresh.php?lat="+str(latitude)+"&lng="+str(longitude)+"&rad="+str(radius)+"&olat=0&olng=0&orad=0&filter=fuel,"+filter)
+    result = conn.getresponse()
+    print result.reason
+    if (result.reason == "OK"):
+        data = result.read()
+        # print "DEBUG - " data
+        json_data = json.loads(data)
+        print json_data
+        for item in json_data:
+            print item.upper()
+        print "DEBUG - " + str(len(json_data['t']))
+        
+    conn.close()
+
+
+
+def main():
+    # results_read()
+    # json_read()
+    generate_result()
+    
+    
 if __name__ == "__main__":
     main()
 
