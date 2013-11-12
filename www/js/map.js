ebe07fc0f8f65accb42d9a60f51de3378a047396
Bryan Jimenez <bjime008@fiu.edu>
2013-11-12 15:14:37 -0500

    setInterval for refresh 10 sec

--- www/js/map.js
+++ www/js/map.js
@@ -421,7 +421,7 @@ function initialize() {
     
  
     
-    setTimeout(refresh,10);
+    setInterval(refresh,10);
 
     map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend.div);
     map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(compass.htmlObj);
