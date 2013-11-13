425770bac75d49eb6a8adddbdaffcbdbd0b9e2ca
Bryan Jimenez <bjime008@fiu.edu>
2013-11-13 17:12:18 -0500

    cleanup markers :)

---
+++ www/cleanup.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+function main(){
+	//echo exec('./cleanup.sh');
+	echo exec('echo "" > /home/jonathan/result.txt');
+}
+
+
+//CALL MAIN
+main();
+
+
+//echo phpinfo();
+//echo 'Last error: ', json_last_error();
+?>
